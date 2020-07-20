c830ee0e57f3863c8ce9988c5cb965fe75cd02f3
Victor <46823972+VictorKobets@users.noreply.github.com>
2020-07-20 23:01:39 +0300

    Update 20.07.2020

--- Test.py
+++ Test.py
@@ -68,32 +68,46 @@ class SomeOperandIsNotTypeDimensions(DimentionsInputTypeError):
     """
 
 
-class D:
+class ChainDimentionsInputTypeError(Exception):
+    """
+    Checking the correctness of the type entered chain of dimension.
+    """
+
+
+class ChainDimensionInputError(ChainDimentionsInputTypeError):
+    """
+    Checking the correctness of the entered chain of dimension.
+    """
+
+
+class A:
     """
     Examples of writing: 5h7, 5.5js6, 10Js7, 15.5H14, 5.5+-0.1, 4.5+0.5, 4.5+0.5-0.02
     """
 
     def __init__(self, dimension):
         if isinstance(dimension, str):
-            self.dimension, self.max, self.min = self.__parser(dimension)
-            self.max += self.dimension
-            self.min += self.dimension
+            self.nominal, self.max, self.min = self.__parser(dimension)
+        elif isinstance(dimension, A):
+            self.nominal, self.max, self.min = dimension.nominal, dimension.max, dimension.min
         else:
             raise DimentionsInputTypeError
     
     def __str__(self):
         return(
-            'Abs: {}\nMax: {}\nMin: {}\nDelta: {}'.format(
-                self.dimension,
+            'Abs: {}\nMax: {}\nMin: {}\nDelta: {}\nTolerance max: {}\nTolerance min: {}'.format(
+                self.nominal,
+                self.nominal + self.max,
+                self.nominal + self.min,
+                self.max - self.min,
                 self.max,
-                self.min,
-                self.max - self.min
+                self.min
             )
         )
     
     def __add__(self, other):
-        if isinstance(other, D):
-            self.dimension += other.dimension
+        if isinstance(other, A):
+            self.nominal += other.nominal
             self.max += other.max
             self.min += other.min
             return self
@@ -102,7 +116,7 @@ class D:
     
     def __mul__(self, num):
         if isinstance(num, int):
-            self.dimension *= num
+            self.nominal *= num
             self.max *= num
             self.min *= num
             return self
@@ -123,57 +137,57 @@ class D:
         Finds dimension and tolerance or throws an exception.
         """
 
-        def tolerance_search(dimension):
+        def tolerance_search(nominal):
             """
             Finds dimension in the database.
             """
-            if Decimal('0.000') < dimension <= Decimal('3.000'):
+            if Decimal('0.000') < nominal <= Decimal('3.000'):
                 return '0/3'
-            elif Decimal('3.000') < dimension <= Decimal('6.000'):
+            elif Decimal('3.000') < nominal <= Decimal('6.000'):
                 return '3/6'
-            elif Decimal('6.000') < dimension <= Decimal('10.000'):
+            elif Decimal('6.000') < nominal <= Decimal('10.000'):
                 return '6/10'
-            elif Decimal('10.000') < dimension <= Decimal('18.000'):
+            elif Decimal('10.000') < nominal <= Decimal('18.000'):
                 return '10/18'
-            elif Decimal('18.000') < dimension <= Decimal('24.000'):
+            elif Decimal('18.000') < nominal <= Decimal('24.000'):
                 return '18/24'
-            elif Decimal('24.000') < dimension <= Decimal('30.000'):
+            elif Decimal('24.000') < nominal <= Decimal('30.000'):
                 return '24/30'
-            elif Decimal('30.000') < dimension <= Decimal('40.000'):
+            elif Decimal('30.000') < nominal <= Decimal('40.000'):
                 return '30/40'
-            elif Decimal('40.000') < dimension <= Decimal('50.000'):
+            elif Decimal('40.000') < nominal <= Decimal('50.000'):
                 return '40/50'
-            elif Decimal('50.000') < dimension <= Decimal('65.000'):
+            elif Decimal('50.000') < nominal <= Decimal('65.000'):
                 return '50/65'
-            elif Decimal('65.000') < dimension <= Decimal('80.000'):
+            elif Decimal('65.000') < nominal <= Decimal('80.000'):
                 return '65/80'
-            elif Decimal('80.000') < dimension <= Decimal('100.000'):
+            elif Decimal('80.000') < nominal <= Decimal('100.000'):
                 return '80/100'
-            elif Decimal('100.000') < dimension <= Decimal('120.000'):
+            elif Decimal('100.000') < nominal <= Decimal('120.000'):
                 return '100/120'
-            elif Decimal('120.000') < dimension <= Decimal('140.000'):
+            elif Decimal('120.000') < nominal <= Decimal('140.000'):
                 return '120/140'
-            elif Decimal('140.000') < dimension <= Decimal('160.000'):
+            elif Decimal('140.000') < nominal <= Decimal('160.000'):
                 return '140/160'
-            elif Decimal('160.000') < dimension <= Decimal('180.000'):
+            elif Decimal('160.000') < nominal <= Decimal('180.000'):
                 return '160/180'
-            elif Decimal('180.000') < dimension <= Decimal('200.000'):
+            elif Decimal('180.000') < nominal <= Decimal('200.000'):
                 return '180/200'
-            elif Decimal('200.000') < dimension <= Decimal('225.000'):
+            elif Decimal('200.000') < nominal <= Decimal('225.000'):
                 return '200/225'
-            elif Decimal('225.000') < dimension <= Decimal('250.000'):
+            elif Decimal('225.000') < nominal <= Decimal('250.000'):
                 return '225/250'
-            elif Decimal('250.000') < dimension <= Decimal('280.000'):
+            elif Decimal('250.000') < nominal <= Decimal('280.000'):
                 return '250/280'
-            elif Decimal('280.000') < dimension <= Decimal('315.000'):
+            elif Decimal('280.000') < nominal <= Decimal('315.000'):
                 return '280/315'
-            elif Decimal('315.000') < dimension <= Decimal('355.000'):
+            elif Decimal('315.000') < nominal <= Decimal('355.000'):
                 return '315/355'
-            elif Decimal('355.000') < dimension <= Decimal('400.000'):
+            elif Decimal('355.000') < nominal <= Decimal('400.000'):
                 return '355/400'
-            elif Decimal('400.000') < dimension <= Decimal('450.000'):
+            elif Decimal('400.000') < nominal <= Decimal('450.000'):
                 return '400/450'
-            elif Decimal('450.000') < dimension <= Decimal('500.000'):
+            elif Decimal('450.000') < nominal <= Decimal('500.000'):
                 return '450/500'
             else:
                 raise DimensionsRangeDoesNotExist
@@ -222,15 +236,87 @@ class D:
                 )
         
         # Finds dimension like this: 4.5+0.5-0.02
-        _type = re.findall(r"^(\d+\.?\d*?)(\+{1}\d+\.?\d*?)(\-{1}\d+\.?\d*?)$", dimension)
+        _type = re.findall(r"^(\d+\.?\d*?)([\+{1}|\-{1}]\d+\.?\d*?)([\-{1}|\+{1}]\d+\.?\d*?)$", dimension)
         if _type != list():
+            print(_type)
+            if Decimal(_type[0][1]) < Decimal(_type[0][2]):
+                a = _type[0][1]
+                b = _type[0][2]
+                a, b = b, a
+            else:
+                a = _type[0][1]
+                b = _type[0][2]
             return(
                 Decimal(_type[0][0]).quantize(Decimal('0.000')),
-                Decimal(_type[0][1]).quantize(Decimal('0.000')),
-                Decimal(_type[0][2]).quantize(Decimal('0.000'))
+                Decimal(a).quantize(Decimal('0.000')),
+                Decimal(b).quantize(Decimal('0.000'))
             )
         else:
             raise DimensionInputError
+
+
+class I(A):
+    """
+    Increasing size inherited from class A, 
+    for more clarity when writing dimension chains.
+    """
+
+    def __init__(self, dimension):
+        super().__init__(dimension)
+
+
+class R(A):
+    """
+    Reducing size inherited from class A,
+    for more clarity when writing dimension chains.
+    """
+
+    def __init__(self, dimension):
+        super().__init__(dimension)
+
+
+class Chain:
+    """
+    Dimension chain representation. Dimension chain solver.
+    Chain view: Chain([I('5h7'), R('2+0.5')]).
+    """
+
+    def __init__(self, chain):
+        if isinstance(chain, list):
+            self.chain = chain
+        else:
+            raise ChainDimentionsInputTypeError
+    
+    def MCI(self):
+        """
+        Method of complete interchangeability.
+        """
+        self.nominal = Decimal('0.000')
+        self.max = Decimal('0.000')
+        self.min = Decimal('0.000')
+        for dimensions in self.chain:
+            if isinstance(dimensions, I):
+                self.nominal += dimensions.nominal
+                self.max += dimensions.max
+                self.min += dimensions.min
+            elif isinstance(dimensions, R):
+                self.nominal -= dimensions.nominal
+                self.max -= dimensions.min
+                self.min -= dimensions.max
+            else:
+                raise ChainDimensionInputError
+        print(
+            'Result of Method of Complete Interchangeability:\n',
+            'Abs: {}\nMax: {}\nMin: {}\nDelta: {}\nTolerance max: {}\nTolerance min: {}'.format(
+                self.nominal,
+                self.nominal + self.max,
+                self.nominal + self.min,
+                self.max - self.min,
+                self.max,
+                self.min
+            ),
+            sep=''
+        )   
 
 
 if __name__ == '__main__':
@@ -244,7 +330,22 @@ if __name__ == '__main__':
     #print('max', c.max)
     #print('min', c.min)
     #print(c)
-    a = D('4.5-0.2')
-    a += D('4.5-0.2')
-    a *= 1
-    print(a)
+    #a = I('4.5-0.2')
+    #a -= I('2.5-0.1')
+    #a *= 1
+    #print(a)
+
+    #c = I('50+0.15') - R('45-0.03') - R('5-0.02')
+    chain = Chain(
+        [
+            I('50+0.15'),
+            R('45-0.03'),
+            R('5-0.02')
+        ]
+    )
+    chain.MCI()
+    #c = I('9-0.25+0.45')
+   # d = Chain([])
+    #print(chain.nominal)
+    #print(chain.max)
+    #print(chain.min)
